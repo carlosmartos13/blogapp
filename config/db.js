ac20731a893843c4b371735e33910bf55f2d89be
carlosmartos13 <carlosmartos04@gmail.com>
2020-05-25 22:58:10 -0300

    make it better

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = { mongURI: "mongodb+srv://blogappnode:blogappnode@321@cluster0-mbsji.mongodb.net/test?retryWrites=true&w=majority"}
+    module.exports = { mongURI: "mongodb+srv://blogappnode:blogappnode@cluster0-mbsji.mongodb.net/test?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
 
